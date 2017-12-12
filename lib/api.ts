a4be7c1e8e4f9a7c837c8c3bcccddb9f47b675ac
Julien <ferjul17@gmail.com>
2017-12-13 00:04:51 +0100

    store cookies in a file

--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,26 @@
 "use strict";
 
 import * as cheerio from "cheerio";
-import {CookieJar} from "request";
+import {access, mkdir, readFile, unlink, writeFile} from "fs";
+import {dirname} from "path";
+import {CookieJar as RequestJar} from "request";
 import * as rp from "request-promise-native";
+import {Cookie} from "tough-cookie";
+import {promisify} from "util";
 import {parseGPUCoreMemory, parseLastUpdate, parseName, parseSpeed, parseTemps} from "./helper";
 import {IGetListRigsRow, IRigInfo} from "./interfaces";
 
 const HOST = "https://simplemining.net/";
+const COOKIE_PATH = __dirname + "/../tmp/.cookies.json";
+const readFileAsync = promisify(readFile);
+const unlinkAsync = promisify(unlink);
+const writeFileAsync = promisify(writeFile);
+const mkdirAsync = promisify(mkdir);
+const accessAsync = promisify(access);
 
 export default class API {
 
-    private jar: CookieJar;
+    private jar: RequestJar;
 
     private email: string;
     private password: string;
@@ -56,7 +66,8 @@ export default class API {
         }));
     }
 
-    private login(email: string, password: string): Promise<CookieJar> {
+    private login(email: string, password: string): Promise<RequestJar> {
+        const jar = rp.jar();
         return rp({
             method: "POST",
             uri: HOST + "account/login",
@@ -65,22 +76,52 @@ export default class API {
                 "data[User][password]": password,
             },
             followAllRedirects: true,
-            jar: this.jar,
+            jar,
             transform: (body) => cheerio.load(body),
         }).then(($) => {
             const message = $("#content-main-notification").text().trim();
             if (!/Logged success/.test(message)) {
                 throw new Error(message);
             }
-            return this.jar;
+            return jar;
         });
     }
 
-    private getJar(): Promise<CookieJar> {
+    private getJar(): Promise<RequestJar> {
         if (this.jar.getCookies(HOST).length) {
             return Promise.resolve(this.jar);
         } else {
-            return this.login(this.email, this.password);
+            const getLoginPromiseFn =
+                (): Promise<RequestJar> => this.login(this.email, this.password).then((jar: RequestJar) => {
+                    this.jar = jar;
+                    const cookieDir = dirname(COOKIE_PATH);
+                    const cookieStr = JSON.stringify(jar.getCookies(HOST));
+                    const writeJarPromiseFn = () => writeFileAsync(COOKIE_PATH, cookieStr).then(() => jar);
+                    const mkdirPromiseFn = () => mkdirAsync(cookieDir).then(writeJarPromiseFn);
+                    return accessAsync(cookieDir).then(writeJarPromiseFn, mkdirPromiseFn);
+                });
+            return readFileAsync(COOKIE_PATH).then((jsonStr: Buffer | string): Promise<RequestJar> => {
+                let json;
+                try {
+                    json = JSON.parse(jsonStr.toString());
+                } catch {
+                    // Ignore error
+                }
+                if (!(json instanceof Array)) {
+                    return Promise.all([unlinkAsync(COOKIE_PATH), getLoginPromiseFn()])
+                        .then(([, theJar]): RequestJar => theJar);
+                }
+                const jar = rp.jar();
+                json.forEach((cookieJSON: object) => {
+                    const cookie = Cookie.fromJSON(cookieJSON);
+                    if (cookie instanceof Cookie) {
+                        // There is an issue in the package @types/request
+                        // Cookie from Request IS Cookie from tough-cookie
+                        jar.setCookie(cookie, HOST);
+                    }
+                });
+                return Promise.resolve(jar);
+            }, getLoginPromiseFn);
         }
     }
 }
